71c53d0308899806306cc2ee663c1479c4b7184f
Your NameDarius <Dariusalexander1118@gmail.com>
2025-03-01 13:52:50 -0800

    location

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState} from 'react'
 import mapboxgl from 'mapbox-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -10,27 +10,74 @@ function App() {
   const mapRef = useRef()
   const mapContainerRef = useRef()
 
-  const center =  [-123.25287720362316, 49.26483429335451]
+  const INITIAL_CENTER =  [-123.25470,49.26550]
+  const INITIAL_ZOOM = 16.4
+  const INITIAL_PITCH = 60
+  
+
+  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+  const [pitch, setPitch] = useState(45)
+
+  
 
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZW5sdWlhIiwiYSI6ImNtN3BuOGVsOTBva3Uyc29ucHRxOG9xMjgifQ.cPVYjBs8F4tZ_TwITYqmEg'
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      center: center,
-      zoom: 13 
+      center: [-123.25470,49.26550],
+      zoom: 16.4,
+      pitch: 60,
       
     });
+
+    mapRef.current.on('move', () => {
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter()
+      const mapZoom = mapRef.current.getZoom()
+      const mapPitch = mapRef.current.getPitch()
+
+      // update state
+      setCenter([ mapCenter.lng, mapCenter.lat ])
+      setZoom(mapZoom)
+
+      // Geolocate
+      mapRef.current.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true
+          },
+          trackUserLocation: true,
+          showUserLocation: true
+        })
+      )
+    })
 
     return () => {
       mapRef.current.remove()
     }
   }, [])
 
+  const handleButtonClick = () => {
+    mapRef.current.flyTo({
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM,
+      pitch: INITIAL_PITCH
+    })
+  }
+  
+
   return (
     <>
+      <div CLASSnAME="sidebar">
+       Longitude: {center [0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
+      <button className ='reset-button' onClick ={handleButtonClick}>
+      Reset
+      </button>
       <div id='map-container' ref={mapContainerRef}/>
     </>
   )
 }
-
+ 
 export default App
